1b8b466f9cd8e6a13717f2251ad4b525dcbec812
Afroza Akter Afrin <afrozaafrin124142@gmail.com>
2025-08-12 23:38:01 +0600

    Refactor database settings to use dj_database_url for PostgreSQL configuration

--- task_management/settings.py
+++ task_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://task_managementdb_oma7_user:PWf1VMoW1A9haq2IPAWh17I3jbWNeNjE@dpg-d2dnflje5dus739p5lkg-a.oregon-postgres.render.com/task_managementdb_oma7',
+        conn_max_age=600
+    )
 }
-
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
